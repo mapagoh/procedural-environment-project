94eb70629d0b7d48f54e9c446e8a329a83b6fbe7
María González <120479987+mapagoh@users.noreply.github.com>
2022-12-24 16:56:06 -0500

    feat: update clean data

--- clean.py
+++ clean.py
@@ -39,6 +39,8 @@ for i in columns:
     data_bgmeasures[i] = data_bgmeasures[i].str.strip()
 
 data_bgmonitoring=pd.read_csv("/data/data_bgmonitoring.csv")
+data_bgmonitoring=data_bgmonitoring.dropna(subset=["monitoring_year"])
+data_bgmonitoring["monitoring_year"]=data_bgmonitoring["monitoring_year"].astype(int)
 data_bgmonitoring.head()
 columns=list(data_bgmonitoring.columns)
 rows=data_bgmonitoring.shape[0]
@@ -93,6 +95,12 @@ for i in columns:
     data_bgpublication[i] = data_bgpublication[i].str.strip()
 
 data_bgreport=pd.read_csv("data/data_bgreport.csv")
+data_bgreport=data_bgreport.dropna(subset=["national_bird_atlas_year"])
+data_bgreport["national_bird_atlas_year"]=data_bgreport["national_bird_atlas_year"].astype(int)
+data_bgreport=data_bgreport.dropna(subset=["national_bird_redlist_year"])
+data_bgreport["national_bird_redlist_year"]=data_bgreport["national_bird_redlist_year"].astype(int)
+data_bgreport=data_bgreport.dropna(subset=["plans_under_prep"])
+data_bgreport["plans_under_prep"]=data_bgreport["plans_under_prep"].astype(int)
 data_bgreport.head()
 columns=list(data_bgreport.columns)
 rows=data_bgreport.shape[0]
@@ -111,6 +119,12 @@ for i in columns:
     data_bgreport[i] = data_bgreport[i].str.strip()
 
 data_birds_check_list=pd.read_csv("data/data_birds_check_list.csv")
+data_birds_check_list=data_birds_check_list.dropna(subset=["euringcode"])
+data_birds_check_list["euringcode"]=data_birds_check_list["euringcode"].astype(int)
+data_birds_check_list=data_birds_check_list.dropna(subset=["assessment_code_EU"])
+data_birds_check_list["assessment_code_EU"]=data_birds_check_list["assessment_code_EU"].astype(int)
+data_birds_check_list=data_birds_check_list.dropna(subset=["assessment_code_trend"])
+data_birds_check_list["assessment_code_trend"]=data_birds_check_list["assessment_code_trend"].astype(int)
 data_birds_check_list.head()
 columns=list(data_birds_check_list.columns)
 rows=data_birds_check_list.shape[0]
@@ -129,6 +143,8 @@ for i in columns:
     data_birds_check_list[i] = data_birds_check_list[i].str.strip()
 
 data_birds_eu_breeding_trends=pd.read_csv("data/data_birds_eu_breeding_trends.csv")
+data_birds_eu_breeding_trends=data_birds_eu_breeding_trends.dropna(subset=["BrRngSizeRnd"])
+data_birds_eu_breeding_trends["BrRngSizeRnd"]=data_birds_eu_breeding_trends["BrRngSizeRnd"].astype(int)
 data_birds_eu_breeding_trends.head()
 columns=list(data_birds_eu_breeding_trends.columns)
 rows=data_birds_eu_breeding_trends.shape[0]
@@ -147,6 +163,8 @@ for i in columns:
     data_birds_eu_breeding_trends[i] = data_birds_eu_breeding_trends[i].str.strip()
 
 data_birds_eu_status=pd.read_csv("data/data_birds_eu_status.csv")
+data_birds_eu_status=data_birds_eu_status.dropna(subset=["PopSizeMax"])
+data_birds_eu_status["PopSizeMax"]=data_birds_eu_status["PopSizeMax"].astype(int)
 data_birds_eu_status.head()
 columns=list(data_birds_eu_status.columns)
 rows=data_birds_eu_status.shape[0]
@@ -183,6 +201,14 @@ for i in columns:
     data_birds_eu_wintering_trends[i] = data_birds_eu_wintering_trends[i].str.strip()
 
 data_birds=pd.read_csv("data/data_birds.csv")
+data_birds=data_birds.dropna(subset=["euringcode"])
+data_birds["euringcode"]=data_birds["euringcode"].astype(int)
+data_birds=data_birds.dropna(subset=["spa_population_min"])
+data_birds["spa_population_min"]=data_birds["spa_population_min"].astype(int)
+data_birds=data_birds.dropna(subset=["spa_population_max"])
+data_birds["spa_population_max"]=data_birds["spa_population_max"].astype(int)
+data_birds=data_birds.dropna(subset=["spa_population_method"])
+data_birds["spa_population_method"]=data_birds["spa_population_method"].astype(int)
 data_birds.head()
 columns=list(data_birds.columns)
 rows=data_birds.shape[0]
@@ -219,6 +245,8 @@ for i in columns:
     data_bmeasures[i] = data_bmeasures[i].str.strip()
 
 data_bpressures_threats=pd.read_csv("data/data_bpressures_threats.csv")
+data_bpressures_threats=data_bpressures_threats.dropna(subset=["quality"])
+data_bpressures_threats["quality"]=data_bpressures_threats["quality"].astype(int)
 data_bpressures_threats.head()
 columns=list(data_bpressures_threats.columns)
 rows=data_bpressures_threats.shape[0]
@@ -237,6 +265,8 @@ for i in columns:
     data_bpressures_threats[i] = data_bpressures_threats[i].str.strip()
 
 species_birds_maes_EU27=pd.read_csv("data/species_birds_maes_EU27.csv")
+species_birds_maes_EU27=species_birds_maes_EU27.dropna(subset=["euringcode"])
+species_birds_maes_EU27["euringcode"]=species_birds_maes_EU27["euringcode"].astype(int)
 species_birds_maes_EU27.head()
 columns=list(species_birds_maes_EU27.columns)
 rows=species_birds_maes_EU27.shape[0]
